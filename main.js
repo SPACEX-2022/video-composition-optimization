430067696999b37edf50a9edc49e0fbf0fd82357
SPACEX-2022 <540240126@qq.com>
2024-04-09 23:06:45 +0800

    commit

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 // Import puppeteer
-import puppeteer from 'puppeteer';
+// import chalk from "chalk";
 import chalk from "chalk";
+import puppeteer from "puppeteer";
 import ora from "ora";
+import * as _ffmpeg from 'fluent-ffmpeg';
+import ff from '@ffmpeg-installer/ffmpeg';
+import * as child_process from "node:child_process";
+
+const ffmpeg = _ffmpeg.default;
+// const chalk = require("chalk");
+// const puppeteer = require('puppeteer');
+// const ora = require("ora");
+// const ffmpeg = require('fluent-ffmpeg');
+// const ff = require('@ffmpeg-installer/ffmpeg');
 
 function log(chalkFunc, ...args) {
     console.log(chalkFunc(...args));
 }
 
+// ffmpeg.setFfmpegPath('C:\\Users\\ASUS\\AppData\\Local\\Microsoft\\WinGet\\Packages\\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\\ffmpeg-7.0-full_build\\bin')
+// ffmpeg
+
+child_process.exec('ffmpeg -version', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+});
 (async () => {
     // Launch the browser
     const browser = await puppeteer.launch({
+        headless: true,
         args: ['--disk-cache'],
     });
 
@@ -51,20 +74,31 @@ function log(chalkFunc, ...args) {
         log(chalk.blue, '视频总时长', totalDuration, 'ms')
 
         let startTimestamp = 0;
+        let index = 0;
         while (startTimestamp !== totalDuration) {
             await seek(startTimestamp);
             let start = Date.now();
-            await page.screenshot({path: `./output/images/${startTimestamp}.png`});
+            await page.screenshot({
+                path: `./output/images/${index}.png`,
+                encoding: 'binary',
+                type: 'jpeg',
+                quality: 80,
+                // fromSurface: true,
+                optimizeForSpeed: true,
+            });
             const saveImgCost = Date.now() - start;
             totalSaveImgCost += saveImgCost;
             log(chalk.yellow, '保存图片耗时', saveImgCost, 'ms')
             startTimestamp += 40;
+            index++;
             if (startTimestamp > totalDuration) {
                 startTimestamp = totalDuration;
             }
         }
 
-        log(chalk.blue, '视频合成总耗时', Date.now() - start, 'ms')
+        const totalCost = Date.now() - start;
+        log(chalk.blue, '视频合成总耗时', totalCost, 'ms')
+        log(chalk.blue, '视频合成比', `1:${(totalCost / totalDuration).toFixed(2)}`, 'ms')
         log(chalk.blue, '- 截帧总耗时', totalSeekCost, 'ms')
 
     } finally {
